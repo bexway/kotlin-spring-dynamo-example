453e26add4c6723a4aa19a534b18d86ccbdf5820
Bex Way <bexzway@gmail.com>
2019-08-20 16:57:38 -0500

    Removed unused Greeting route

--- src/main/kotlin/com/shoprunner/kotlinspringdynamoexample/GreetingController.kt
+++ src/main/kotlin/com/shoprunner/kotlinspringdynamoexample/GreetingController.kt
@@ -1,10 +1,8 @@
 package com.shoprunner.kotlinspringdynamoexample
 
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
-import java.util.concurrent.atomic.AtomicLong
 
 import com.shoprunner.kotlinspringdynamoexample.dynamo.UserRepository
 import com.shoprunner.kotlinspringdynamoexample.dynamo.User
@@ -14,12 +12,6 @@ import com.shoprunner.kotlinspringdynamoexample.dynamo.UserId
 class GreetingController(
     val userRepository: UserRepository
 ) {
-    val counter = AtomicLong()
-
-    @GetMapping("/greeting")
-    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
-        Greeting(counter.incrementAndGet(), "Hello, $name")
-
     @GetMapping("/user/{firstName}")
     fun user(@PathVariable firstName: String): List<User> =
         userRepository.findByFirstName(firstName)
@@ -31,7 +23,6 @@ class GreetingController(
     @GetMapping("/delete/{firstName}/{lastName}")
     fun delete(@PathVariable firstName: String, @PathVariable lastName: String) =
         userRepository.deleteById(UserId(firstName, lastName))
-
 
     @GetMapping("/deleteall")
     fun deleteAll() =
